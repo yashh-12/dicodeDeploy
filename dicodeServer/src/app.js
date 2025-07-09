580bdcb870198c64466f5dd6a9a15f3ec926873a
yashh-12 <yashvantboricha01.com>
2025-07-09 16:41:55 +0530

    changes in env

--- dicodeServer/src/app.js
+++ dicodeServer/src/app.js
@@ -25,7 +25,7 @@ const app = express();
 const http = createServer();
 const server = new Server(http, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.CORS,
         methods: ["GET", "POST"]
     }
 });
